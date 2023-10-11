1c5609dce3561ee087adf7906d7364d9bdd5a60f
William <byweimar@gmail.com>
2023-10-10 20:59:15 -0400

    Func Commit

--- funcs.js
+++ funcs.js
@@ -1,13 +1,20 @@
 function isFive(num) {
-  // Your code here
+  return num === 5;
 }
 
 function isOdd(number) {
-  // Your code here
+  if (typeof number !== "number") {
+    throw TypeError("Must be a number");
+  }
+  return number % 2 !== 0;
 }
 
 function myRange(min, max, step = 1) {
-  // Your code here
+  const newArr = [];
+  for (let i = min; i <= max; i += step) {
+    newArr.push(i);
+  }
+  return newArr;
 }
 
 
